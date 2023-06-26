210d023a6d81e6b821b119a41ee87af65a922f7c
zzj29 <133968225+zzj29@users.noreply.github.com>
2023-06-26 20:33:52 +1000

    Update enquiry.js

--- enquiry.js
+++ enquiry.js
@@ -3,7 +3,7 @@
 function fnDay() {
   document.getElementById("txtResult").value = "You have chosen day class";
 }
-function fnnight() {
+function fnNight() {
   document.getElementById("txtResult").value = "You have chosen night class";
 }
 //......................................................................
@@ -13,41 +13,47 @@ var radio2 = document.getElementById("radionight");
 //......................................................................
 // use the onclick event handler to go to the functions
 radio1.onclick = fnDay;
-radio2.onclick = fnnight;
+radio2.onclick = fnNight;
 
 
 // Validate the value of required inputs
 function fnCheckInput() {
-inputValid = true;
-if (FirstName.value.length < 2) {
-  alert("Please enter your first name");
-  inputValid = false;
-};
-  if (LastName.value.length < 2) {
-  alert("Please enter your last name");
-  inputValid = false;
-};
-if (userEmail.value.indexOf("@")=="-1" || userEmail.value.indexOf(".")=="-1") {
-  alert("Please enter a valid Email address");
-  inputValid = false;
-};
-  if (userMobile.value.length != 10) {
-  alert("Please enter a 10 digits mobile number");
-  inputValid = false;
-};
-if (inputValid) {
+
+  // create variables to store the path 
+  var FirstName = document.getElementById("txtfirstName").value;
+  var LastName = document.getElementById("txtLastName").value;
+  var email = document.getElementById("txtEmail").value;
+  var mobile = document.getElementById("txtMobile").value;
+  //......................................................................
+
+  if (FirstName.length < 2) {
+    alert("Please enter your first name");
+    return false;
+  };
+    if (LastName.length < 2) {
+    alert("Please enter your last name");
+    return false;
+  };
+  if (email.indexOf("@")=="-1" || email.indexOf(".")=="-1") {
+    alert("Please enter a valid Email address");
+    return false;
+  };
+    if (mobile.length != 10 || isNaN(mobile)) {
+    alert("Please enter a 10 digits mobile number");
+    return false;
+  };
+  
   alert("Thank you for submitting the enquiry form");
-  form2.action = form2.action + ";" + userEmail.value;
-}
+  form2.action = form2.action + ";" + email;
+  return true;
 }
 //......................................................................
-// create variables to store the path 
+
+// Create variables to store the form and input elements
 var form2 = document.getElementById("form2");
-var FirstName = document.getElementById("txtfirstName");
-var LastName = document.getElementById("txtLastName");
-var userEmail = document.getElementById("txtEmail");
-var userMobile = document.getElementById("txtMobile");
-//......................................................................
-/* use onchange event handler to catch the focus of browser when textbox value changes, and call a function named 'fnUpperMe' */
+
+// Assign the validateForm function to the onsubmit event of the form
 form2.onsubmit = fnCheckInput;
+
+// Set focus on the first name input field when the page loads
 FirstName.focus();
